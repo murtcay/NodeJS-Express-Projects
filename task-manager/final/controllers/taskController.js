1a4f627ec05194c25f46a212f071c057a74b2ffc
Murat ÇAY <murtcay@gmail.com>
2021-11-20 22:44:18 +0300

    task manager app: Delete the task.

--- task-manager/final/controllers/taskController.js
+++ task-manager/final/controllers/taskController.js
@@ -26,7 +26,7 @@ const getSingleTask = async (req, res) => {
     const task = await Task.findOne({ _id: taskID });
     
     if(!task) {
-      return res.status(500).json({ error: `There is no task with id: ${taskID}`});
+      return res.status(404).json({ error: `There is no task with id: ${taskID}`});
     }
 
     res.status(200).json({ task });
@@ -43,11 +43,23 @@ const updateTask = (req, res) => {
   });
 };
 
-const deleteTask = (req, res) => {
-  res.json({
-    id: req.params.id,
-    action: 'delete task'
-  });
+const deleteTask = async (req, res) => {
+  try {
+    const taskID = req.params.id;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    
+    if(!task) {
+      return res.status(404).json({ error: `There is no task with id: ${taskID}`});
+    }
+
+    res.status(200).json({
+      tatasksk: null,
+      status: 'successfully deleted'
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 
