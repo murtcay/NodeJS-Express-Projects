26b5b06354281391da638d855e1893a1f60304b9
Murat ÇAY <murtcay@gmail.com>
2021-11-20 23:11:50 +0300

    task manager app: Update the task with PATCH method. Options passed to findOneAndUpdate helper function.

--- task-manager/final/controllers/taskController.js
+++ task-manager/final/controllers/taskController.js
@@ -22,7 +22,7 @@ const createTask = async (req, res) => {
 
 const getSingleTask = async (req, res) => {
   try {
-    const taskID = req.params.id;
+    const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
     
     if(!task) {
@@ -36,16 +36,28 @@ const getSingleTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.json({
-    id: req.params.id,
-    action: 'update task'
-  });
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if(!task) {
+      return res.status(404).json({ error: `There is no task with id: ${taskID}`});
+    }
+
+    res.status(200).json({ task });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const deleteTask = async (req, res) => {
   try {
-    const taskID = req.params.id;
+    const { id: taskID } = req.params;
     const task = await Task.findOneAndDelete({ _id: taskID });
     
     if(!task) {
