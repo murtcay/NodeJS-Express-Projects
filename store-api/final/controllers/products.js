525a99f05342b07b576aca387e6dea8d277ae501
Murat ÇAY <murtcay@gmail.com>
2021-11-21 17:10:06 +0300

    store api: pagination with skip and limit.

--- store-api/final/controllers/products.js
+++ store-api/final/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(2);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -24,18 +28,23 @@ const getAllProducts = async (req, res) => {
   let result = Product.find(queryObject);
 
   if(sort) {
-    console.log(sort);
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   }
   else {
     result = result.sort('createdAt');
   }
-  console.log('fields: ', fields);
+
   if(fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit)
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
