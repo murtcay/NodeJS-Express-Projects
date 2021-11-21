6ca6f51423ca3c9e741cda7135457226ff9dfa9a
Murat ÇAY <murtcay@gmail.com>
2021-11-21 16:47:15 +0300

    store api: sort - getAllProducts implementation.

--- store-api/final/controllers/products.js
+++ store-api/final/controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' }
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if(featured) {
@@ -23,10 +20,19 @@ const getAllProducts = async (req, res) => {
   if(name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
+  
+  let result = Product.find(queryObject);
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if(sort) {
+    console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  }
+  else {
+    result = result.sort('createdAt');
+  }
 
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
