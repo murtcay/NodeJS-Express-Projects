504a28a68f63155605eea0e9606ca788691779fb
Murat ÇAY <murtcay@gmail.com>
2021-11-21 17:46:26 +0300

    store api: numericFilters added.

--- store-api/final/controllers/products.js
+++ store-api/final/controllers/products.js
@@ -1,16 +1,14 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(10)
-    .skip(2);
+  const products = await Product.find({price: { $gt: 30 }})
+    .sort('price')
+    .select('name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if(featured) {
@@ -23,6 +21,31 @@ const getAllProducts = async (req, res) => {
 
   if(name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+  console.log('numericFilters: ', numericFilters);
+  if(numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+
+    const regEx = /\b(<|>|<=|=|>=)\b/g;
+    
+    let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+    const options = ['price', 'rating'];
+    
+    filters = filters.split(',').forEach((item) => {
+      console.log('item: ', item);
+      const [field, operator, value] = item.split('-');
+
+      if(options.includes(field)) {
+        queryObject[field] = {[operator]: Number(value)};
+      }
+    });
+
   }
   
   let result = Product.find(queryObject);
@@ -44,7 +67,7 @@ const getAllProducts = async (req, res) => {
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
-  result = result.skip(skip).limit(limit)
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
