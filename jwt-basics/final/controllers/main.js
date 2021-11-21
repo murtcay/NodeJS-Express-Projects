c0a539f994ba583791f5f61ba73221e7d66aa471
Murat ÇAY <murtcay@gmail.com>
2021-11-21 23:40:08 +0300

    jwt basics: sign jwt.

--- jwt-basics/final/controllers/main.js
+++ jwt-basics/final/controllers/main.js
@@ -6,6 +6,7 @@
  * setup authentication so only the request with JWT can access the dashboard
  * 
  */
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error'); 
 
 const login = async (req, res) => {
@@ -15,7 +16,21 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide email and password', 400);
   }
 
-  res.send('Fake Login/Register/Signup Route');
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getTime();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value !!!
+  const token = jwt.sign(
+    { id, username }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: '30d' }
+  );
+  
+  res.status(200).json({
+    msg: 'user created',
+    token
+  });
 };
 
 const dashboard = async (req, res) => {
