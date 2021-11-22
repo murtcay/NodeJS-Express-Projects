b025acdedcfff027126c94284f7b9a1a6132ad22
Murat ÇAY <murtcay@gmail.com>
2021-11-22 14:56:12 +0300

    jobs api: mongoDB connection and user model setup.

--- jobs-api/final/app.js
+++ jobs-api/final/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 // connect DB
+const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/auth');
@@ -27,6 +28,8 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('CONNECTED TO DB!!!');
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
