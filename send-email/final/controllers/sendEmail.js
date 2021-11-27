36cdb123ba29234baa4878cbce2c015ee8279268
Murat ÇAY <murtcay@gmail.com>
2021-11-27 22:31:28 +0300

    send-email: send email with sendgrid .

--- send-email/final/controllers/sendEmail.js
+++ send-email/final/controllers/sendEmail.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
 const sendEmailEthereal = async (req, res) => {
 
@@ -30,6 +31,23 @@ const sendEmailEthereal = async (req, res) => {
 };
 
 const sendEmailSendGrid = async (req, res) => {
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: 'mailtestuser8@gmail.com', // Change to your recipient
+      from: process.env.MAIL_ACCOUNT, // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      html: 'Hello from <strong>Node.js</strong>. Sending Email using SendGrid',
+    };
+    
+    const info = sgMail.send(msg);
+    res.json(info);
+
+  } catch (err) {
+    res.json({
+      error: err.message
+    });
+  }
   res.send('Email Sendgrid');
 };
 
