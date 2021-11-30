d7c9f75e13d0026d720daa256fe7bf8757877476
Murat ÇAY <murtcay@gmail.com>
2021-11-30 16:38:58 +0300

    e-commerce api: Register controller and handling password.

--- e-commerce-api/final/controllers/authController.js
+++ e-commerce-api/final/controllers/authController.js
@@ -1,5 +1,25 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const { 
+    email,
+    name,
+    password 
+  } = req.body;
+
+  const emailAlredyExists = await User.findOne({ email });
+  if(emailAlredyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = await User.countDocuments({});
+  const role = (isFirstAccount === 0) ? 'admin' : 'user'; 
+
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
