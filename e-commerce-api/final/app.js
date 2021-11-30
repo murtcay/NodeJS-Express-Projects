0518e05631320897a5d6d780a677ae0a901b7417
Murat ÇAY <murtcay@gmail.com>
2021-11-30 14:57:52 +0300

    e-commerce api: Basic routes and middleware.

--- e-commerce-api/final/app.js
+++ e-commerce-api/final/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 // express
 const express = require('express');
 const app = express();
@@ -6,6 +7,18 @@ const app = express();
 // database
 const connectDB = require('./db/connect');
 
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('E-commerce API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
