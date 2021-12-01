910b2422071a6a16cd0f019316a0bf261f69f5c1
Murat ÇAY <murtcay@gmail.com>
2021-12-01 21:27:34 +0300

    e-commerce api: show current user route setup.

--- e-commerce-api/final/routes/userRoutes.js
+++ e-commerce-api/final/routes/userRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
