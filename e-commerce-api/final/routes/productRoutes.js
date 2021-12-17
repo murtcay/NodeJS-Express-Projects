09924a9abf77e29b718ebb896eea9ca7c12b21f7
Murat ÇAY <murtcay@gmail.com>
2021-12-17 15:12:36 +0300

    Product routes and createProduct controller added.

---
+++ e-commerce-api/final/routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { model } = require('mongoose');
+const router = express.Router();
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProducts,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require('../controllers/productController');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+
+router
+  .route('/')
+  .get(getAllProducts)
+  .post([authenticateUser, authorizePermissions('admin')], createProduct);
+
+router
+  .route('/uploadImage')
+  .post(authenticateUser, authorizePermissions('admin'), uploadImage);
+
+router
+  .route('/:id')
+  .get(getSingleProducts)
+  .patch(authenticateUser, authorizePermissions('admin'), updateProduct)
+  .delete(authenticateUser, authorizePermissions('admin'), deleteProduct);
+
+module.exports = router;
