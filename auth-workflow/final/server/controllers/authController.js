ad0d88756422c99a5c2ca0b163e8f2947bbefb95
Murat ÇAY <murtcay@gmail.com>
2022-01-06 00:27:22 +0300

    check for existing token, refactor auth middleware: accessToken and refreshToken

--- auth-workflow/final/server/controllers/authController.js
+++ auth-workflow/final/server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const {
@@ -82,16 +83,39 @@ const login = async (req, res) => {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
   const isPasswordCorrect = await user.comparePassword(password);
+
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
-
   if (!user.isVerified) {
-    throw new CustomError.UnauthenticatedError('Please verify your email.');
+    throw new CustomError.UnauthenticatedError('Please verify your email');
+  }
+  const tokenUser = createTokenUser(user);
+
+  // create refresh token
+  let refreshToken = '';
+  // check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
   }
 
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
+  refreshToken = crypto.randomBytes(40).toString('hex');
+  const userAgent = req.headers['user-agent'];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  await Token.create(userToken);
+
+  attachCookiesToResponse({ res, user: tokenUser, refreshToken });
 
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
